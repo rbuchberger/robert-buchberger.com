c0bb2bd1dff9242776f4e0e9ff20d9dfaa587619
Robert Buchberger <rbuchberger@gmail.com>
2018-09-11 13:43:26 +0200

    created responsive nav menu

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const navMenu = document.querySelector('nav.main ul');
+const menuButton = document.querySelector('.hamburger');
+
+function toggleMenu() {
+  navMenu.classList.toggle('hidden-mobile');
+  menuButton.classList.toggle('active');
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+// Progressive enhancement: Show the nav menu and hide the button by default, switch them o ut once we know javascript works. 
+navMenu.classList.add('hidden-mobile');
+menuButton.classList.remove('hidden-mobile');
