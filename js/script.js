d7ac71ec0328756cbf2ef41f4a792ec530593e75
Robert Buchberger <rbuchberger@gmail.com>
2018-11-05 20:26:41 +0100

    basic page-load animation functionality

--- js/script.js
+++ js/script.js
@@ -11,3 +11,6 @@ menuButton.addEventListener('click', toggleMenu);
 // Progressive enhancement: Show the nav menu and hide the button by default, switch them o ut once we know javascript works. 
 navMenu.classList.add('hidden-mobile');
 menuButton.classList.remove('hidden-mobile');
+
+// Enable swup
+const swup = new Swup();
