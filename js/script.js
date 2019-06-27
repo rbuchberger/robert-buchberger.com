a4f6d7d4f088b7583bbfd903d60765e93731f37d
Robert Buchberger <robert@buchberger.cc>
2019-06-27 16:09:23 +0200

    format javascript

--- js/script.js
+++ js/script.js
@@ -1,77 +1,77 @@
 // import Swup from '/js/swup.min.js';
 // eslint no-unused-vars: off, no-undef: off
-const navMenu = document.querySelector('nav.main');
-const navMenuList = document.querySelector('nav.main ul');
-const menuButton = document.querySelector('.hamburger');
-const navTab = document.querySelector('div.navbar-tab');
+const navMenu = document.querySelector('nav.main')
+const navMenuList = document.querySelector('nav.main ul')
+const menuButton = document.querySelector('.hamburger')
+const navTab = document.querySelector('div.navbar-tab')
 
 // DOMContentLoaded triggers when loading page the first time or on refresh.
-window.addEventListener('load', instantNavUpdate);
+window.addEventListener('load', instantNavUpdate)
 
 // swup:willReplaceContent triggers when a link is clicked. This allows the
 // animation to start instantly, rather than waiting for new content to
 // download.
-document.addEventListener('swup:clickLink', navUpdate);
+document.addEventListener('swup:clickLink', navUpdate)
 
 // swup:pageView triggers when content is displayed. Triggering a second time
 // accounts for scroll bar appearing or disappearing on page load. Without this
 // call, highlight ends up in the wrong place if the scrollbar is added or removed
 // between pages.
-document.addEventListener('swup:pageView', navUpdate);
+document.addEventListener('swup:pageView', navUpdate)
 
 // We need to reposition the highlight when the window is resized.
-window.addEventListener('resize', resizeThrottled);
+window.addEventListener('resize', resizeThrottled)
 
 // Close the nav menu when a link is selected.
-document.addEventListener('swup:clickLink', closeMenu);
+document.addEventListener('swup:clickLink', closeMenu)
 
 // Add functionality to the nav menu.
-menuButton.addEventListener('click', toggleMenu);
+menuButton.addEventListener('click', toggleMenu)
 
 // Enable Swup
-new Swup();
+new Swup()
 
-let throttled;
+let throttled
 
 function resizeThrottled() {
-  if (throttled) return;
+  if (throttled) return
 
-  instantNavUpdate();
+  instantNavUpdate()
 
-  throttled = true;
+  throttled = true
   setTimeout(() => {
-    throttled = false;
+    throttled = false
 
     // If another resize happens while throttled, it'll end up in the wrong
     // place. This fixes that:
-    instantNavUpdate();
-  }, 100);
+    instantNavUpdate()
+  }, 100)
 }
 
 // Position highlight div, without transition effects.
 function instantNavUpdate() {
-  navTab.classList.remove('slide-transition');
-  navUpdate();
+  navTab.classList.remove('slide-transition')
+  navUpdate()
   // Wait to enable transitions till we're sure it's in the right place:
-  window.setTimeout(enableHighlightTransition, 300);
+  window.setTimeout(enableHighlightTransition, 300)
 }
 
 function enableHighlightTransition() {
-  navTab.classList.add('slide-transition');
+  navTab.classList.add('slide-transition')
 }
 
 function toggleMenu() {
-  navMenu.classList.toggle('open');
-  menuButton.classList.toggle('active');
+  navMenu.classList.toggle('open')
+  menuButton.classList.toggle('active')
 }
 
 function closeMenu() {
   window.setTimeout(() => {
-    navMenu.classList.remove('open');
-    menuButton.classList.remove('active');
+    navMenu.classList.remove('open')
+    menuButton.classList.remove('active')
   },
   150
-  );
+  )
 }
 
 function navUpdate(e) {
@@ -82,31 +82,31 @@ function navUpdate(e) {
     e && e.currentTarget.activeElement.href ?
       e.currentTarget.activeElement.href :
       window.location.href
-  ).pathname);
+  ).pathname)
 
   // Grab all nav items
-  const navItems = Array.from(navMenuList.children);
+  const navItems = Array.from(navMenuList.children)
 
   // Grab appropriate nav item
   const activeItem = navItems.find(item => {
     return getBasePath(item.firstElementChild.attributes.href.value) ===
-      targetPath;
-  }) || navItems[0];
+      targetPath
+  }) || navItems[0]
 
   // Set target position of div
-  const targetPosition = findTargetPosition(activeItem);
+  const targetPosition = findTargetPosition(activeItem)
 
   // Position Div
-  updateHighlightPosition(targetPosition);
+  updateHighlightPosition(targetPosition)
 
   // Style nav items
   navItems.forEach(item => {
     if (item === activeItem) {
-      item.classList.add('active');
+      item.classList.add('active')
     } else {
-      item.classList.remove('active');
+      item.classList.remove('active')
     }
-  });
+  })
 }
 
 function findTargetPosition(activeItem) {
@@ -115,27 +115,27 @@ function findTargetPosition(activeItem) {
     height: activeItem.offsetHeight,
     top: activeItem.offsetTop + navMenu.offsetTop,
     left: activeItem.offsetLeft + navMenu.offsetLeft
-  };
+  }
 }
 
 function Transformation(targetPosition) {
   // We add a little fudge factor to the X scale to prevent ugly white gaps
   // between the outline and the highlight.
-  this.scaleX = .01 + (targetPosition.width / navTab.offsetWidth);
-  this.scaleY = targetPosition.height / navTab.offsetHeight;
-  this.translateY = (targetPosition.top - navTab.offsetTop - navMenu.offsetTop);
-  this.translateX = (targetPosition.left - navTab.offsetLeft - navMenu.offsetLeft);
+  this.scaleX = .01 + (targetPosition.width / navTab.offsetWidth)
+  this.scaleY = targetPosition.height / navTab.offsetHeight
+  this.translateY = (targetPosition.top - navTab.offsetTop - navMenu.offsetTop)
+  this.translateX = (targetPosition.left - navTab.offsetLeft - navMenu.offsetLeft)
 }
 
 function updateHighlightPosition(targetPosition) {
-  const trans = new Transformation(targetPosition);
+  const trans = new Transformation(targetPosition)
   navTab.style.transform =
-    `translate(${trans.translateX}px, ${trans.translateY}px) scale(${trans.scaleX}, ${trans.scaleY}) `;
+    `translate(${trans.translateX}px, ${trans.translateY}px) scale(${trans.scaleX}, ${trans.scaleY}) `
 }
 
 // Given a relative pathname, determine site base position. Example: Given
 // 'blog.html', return 'blog', given '/', return '', given
 // '/blog/2018/aviation-and-programming.html' return 'blog'
 function getBasePath(url) {
-  return /^\/?(\w*)[/.]?/.exec(url)[1];
+  return /^\/?(\w*)[/.]?/.exec(url)[1]
 }
